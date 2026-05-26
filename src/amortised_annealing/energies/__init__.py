e5538f4116a1a61d9631517a3260d4df1873b002
siddhantgadkari <svg21@ic.ac.uk>
2026-05-26 14:09:54 +0100

    Add __init__ to energies

---
+++ src/amortised_annealing/energies/__init__.py
@@ -0,0 +1,13 @@
+from .base import Energy, BoltzmannDistribution
+from .double_well import DoubleWell, ManyWell
+from .ackley import Ackley
+from .rastrigin import Rastrigin
+
+__all__ = [
+    "Energy",
+    "BoltzmannDistribution",
+    "DoubleWell",
+    "ManyWell",
+    "Ackley",
+    "Rastrigin",
+]
